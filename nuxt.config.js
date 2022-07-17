c78ded1ea75805e6840f75d250f06d40bae28473
LAPTOP-BTR7T3QU\G1 <jangji1@gmail.com>
2022-07-17 21:37:04 +0900

    fix: base router

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,10 +5,6 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
-  router: {
-    base: '/portfolio/',
-  },
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: '장지원입니다',
